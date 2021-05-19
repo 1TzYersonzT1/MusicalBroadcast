7a33a4d2555ede201c42de203d8c622ae01263a2
Jorge Valdes Narvaez <jorge.vnarvaez@gmail.com>
2021-05-19 17:05:58 -0400

    usuario_rol migration added

---
+++ database/migrations/2021_05_19_202824_create_usuario_rol_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarioRolTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario_rol', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('user');
+            $table->foreignId('rol_id')->constrained('rol');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario_rol');
+    }
+}
