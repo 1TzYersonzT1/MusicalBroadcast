937fe5e7ff76374fd3df29ba8b64df0529312a2b
unknown <yersonherrera14@gmail.com>
2021-06-12 00:22:54 -0400

    artista preview avanzado

--- resources/views/livewire/artista/artista-preview.blade.php
+++ resources/views/livewire/artista/artista-preview.blade.php
@@ -1,29 +1,50 @@
 <div>
+    <div class="h-full flex-none bg-cover bg-no-repeat w-full static bottom-0 left-10 "style="background-image: url('https://mexico-grlk5lagedl.stackpathdns.com/production/mexico/images/1547572747299933-Carlos-Maycotte.jpg?w=1920&h=800&fit=crop&crop=focalpoint&auto=%5B%22format%22%2C%20%22compress%22%5D&cs=srgb')">
+        <div class="flex lg:flex-row justify-around flex-col py-6 h-full ">
+            <div class="text-white px-10 py-10 text-center">
+                <div class=" h-60 lg:w-60 flex-none bg-cover rounded-full lg:rounded-t-full lg:rounded-1 text-center overflow-hidden"
+                    style="background-image: url('https://tailwindcss.com/img/card-left.jpg')" title="Woman holding a mug">
+                </div>
+                <div class="mb-4 text-4xl font-bold ">
 
-    <div>
-        <div class="text-white px-10 text-center">
-            <div class="mb-4 text-4xl font-bold">
+                    {{ $artistaActual->ART_Nombre }}
 
-                {{ $artistaActual->ART_Nombre }}
-
+                </div>
+                <div class="py-2">
+                    @if ($artistaActual->tipo_artista == 1)
+                        Solista
+                    @endif
+                    @if ($artistaActual->tipo_artista == 0)
+                        Banda
+                    @endif
+                    
+                </div>
             </div>
         </div>
     </div>
 
+                    
 
-    <div class="flex lg:flex-row justify-between flex-col py-8 min-h-screen">
+
+
+
+    <div class="flex lg:flex-row justify-around flex-col py-6 h-full">
 
 
 
         <div class="px-5 py-2 ">
-            <div class="text-white ">
-                <div class="bg-black px-5 py-1">
-                    <div class="top-5 mb-4 text-4xl font-bold ">
+            <div class="text-white px-5">
+                <div class="bg-black px-2 py-1 relative">
+                    <div class="top-5 mb-3 text-4xl font-bold">
                         Discografía
+                        
                     </div>
                 </div>
                 <div>
-            
+                     <div class=" h-40 lg:w-40 flex-none bg-cover rounded-full lg:rounded-t-full lg:rounded-1 text-center overflow-hidden"
+                    style="background-image: url('https://tailwindcss.com/img/card-left.jpg')" title="Woman holding a mug">
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -31,28 +52,21 @@
 
 
 
-        <div class="lg:mb-0 mb-10 h-20 lg:w-50 border-1  py-2">
-            <div class="text-white px-10 ">
-                <div class="bg-black px-5 py-1">
-                    <div class="mb-4 text-4xl font-bold ">
+        <div class="py-2 ">
+            <div class="text-white px-flex ">
+                <div class="bg-black px-4 py-1">
+                    <div class="mb-3 text-4xl font-bold">
                         Musica
                     </div>
                 </div>
-                <a href="https://www.spotify.com/bo/">
-                <div>Spotify
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor" >
-                        <path 
-                            d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
+                <div ><br>
+                    <div>
+                        <iframe src="https://open.spotify.com/follow/1/?uri=spotify:artist:4zig82wPnO2yb43Lx4x8s6?si=RaGP9uHPTDu-UBlrrVzSNQ&dl_branch=1&size=detail&theme=dark&show-count=0" width="300" height="56" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowtransparency="true"></iframe>
+                    </div>
+                    <br>
+                    <div class="g-ytsubscribe" data-channelid="UCpLP_h0sHPynXomznPGto2g" data-layout="full" data-theme="dark" data-count="hidden">
                     
-                    </svg>
-                    </a>
-                    <a href="https://www.youtube.com/">Youtube
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
-                    
-                        <path
-                            d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
-                    </svg>
-                    </a>
+                    </div>  
                 </div>
             </div>
         </div>
@@ -65,47 +79,39 @@
 
 
 
-    <div class="flex lg:flex-row justify-between flex-col py-8 min-h-0">
+    <div class="flex lg:flex-row justify-around flex-col py-8 h-full">
 
-        <div>
-            <div class="lg:mb-0 mb-10 h-full lg:w-50 border-1 px-5 py-2">
-                <div class="text-white px-10">
-                    <div class="bg-black px-5 py-1">
-                        <div class="mb-4 text-4xl font-bold">
+        
+            <div class="px-5 py-2 ">
+                <div class="text-white px-5">
+                    <div class="bg-black px-2 py-1">
+                        <div class="top-5 mb-3 text-4xl font-bold ">
                             Biografia
                         </div>
                     </div>
+
                     <div>
                         {{ $artistaActual->biografia }}
                     </div>
                 </div>
             </div>
-        </div>
+        
 
 
-        <div>
-            @if ($artistaActual->tipo_artista == 1)
-                <div class="text-white px-10">
+    
+            <div class=" py-2 ">
+                <div class="text-white px-flex">
                     <div class="bg-black px-5 py-1">
-                        <div class="mb-4 text-4xl font-bold">
-                            Solista
+                        <div class="top-5 mb-3 text-4xl font-bold ">
+                             Proximo<br>
+                             Evento
 
                         </div>
                     </div>
                 </div>
-            @endif
-            @if ($artistaActual->tipo_artista == 0)
-                <div class="text-white px-10">
-                    <div class="bg-black px-5 py-1">
-                        <div class="mb-4 text-4xl font-bold">
-                         Banda
-
-                        </div>
-                    </div>
-                </div>
-            @endif
-        </div>
+            </div>
 
     </div>
 
 </div>
+<script src="https://apis.google.com/js/platform.js"></script>
